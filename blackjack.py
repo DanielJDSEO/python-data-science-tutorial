fc54b957e73f8c9b87d34a6211788fcb0d50ecf7
Daniel <djd100@comcast.net>
2022-06-04 17:20:55 -0700

    Created Shoe Class

--- blackjack.py
+++ blackjack.py
@@ -5,6 +5,10 @@
 
 #Card attributes are set, and so are deck ones. Now, what behaviors does a Deck have?
 #A deck should be able to be dealt from, so a draw function should be possible.
+#Now I want to create a shoe object to hold the necessary number of decks for one simulation
+#I want to ensure I can simulate several shoes per execution of the code with a summarization of the results at the shoe level.
+
+#I don't believe a Deck class is necessary, as the Shoe class totally encapsulates its functionality
 
 import itertools
 import random
@@ -21,28 +25,38 @@ class Card:
 
 
 class Deck:
-    
-    new_deck = list(itertools.product(Card.list_of_values,Card.list_of_suits))
-    random.shuffle(new_deck)
 
     def __init__ (self):
-        pass
+        self.new_deck = list(itertools.product(Card.list_of_values,Card.list_of_suits))
+        random.shuffle(self.new_deck)
 
     def draw_card(self):
         Card = self.new_deck.pop()
         print(Card)
         return(Card)
 
-Deck_1 = Deck()
 
-# Deck_1.draw_card()
-# Deck_1.draw_card()
+class Shoe:
 
-cards_burned = 0
+    def __init__(self,number_of_decks = 1,deck_penetration = 1.0):
+        self.number_of_decks = number_of_decks
+        self.deck_penetration = deck_penetration
+        self.new_shoe = []
+            
+        for self.decks in range(0,number_of_decks):
+            self.new_shoe.append(Deck())
 
-for cards in range(0,50): 
-    Deck_1.draw_card()
-    cards_burned += 1
-print(len(Deck_1.new_deck))
-print(Deck_1.new_deck)
-print(cards_burned)
+
+#Deck_1 = Deck()
+
+# cards_burned = 0
+
+# for cards in range(0,50): 
+#     Deck_1.draw_card()
+#     cards_burned += 1
+#print(len(Deck_1.new_deck))
+# print(Deck_1.new_deck)
+# print(cards_burned)
+
+Shoe_1=Shoe()
+print(len(Shoe_1.new_shoe(Deck())))
