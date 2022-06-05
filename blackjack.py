bca3f660bcb25886ae88a41895579748dd3fd96f
Daniel <djd100@comcast.net>
2022-06-04 18:03:34 -0700

    Shoe Class Complete, next up is Player & Dealer

--- blackjack.py
+++ blackjack.py
@@ -1,18 +1,23 @@
 
 #I want to recreate the game of Blackjack. I will need to use object-oriented programming to do so.
-#Think about the different real-world objects in the game. There is a Deck, Cards that constitute the Deck, Players that use the Deck, and a Dealer.
-#I will construct the Cards first, since that will constitute the Deck
+# Think about the different real-world objects in the game. There is a Deck, Cards that constitute the Deck, Players that use the Deck, and a Dealer.
+# I will construct the Cards first, since that will constitute the Deck
 
 #Card attributes are set, and so are deck ones. Now, what behaviors does a Deck have?
-#A deck should be able to be dealt from, so a draw function should be possible.
-#Now I want to create a shoe object to hold the necessary number of decks for one simulation
-#I want to ensure I can simulate several shoes per execution of the code with a summarization of the results at the shoe level.
+# A deck should be able to be dealt from, so a draw function should be possible.
+# Now I want to create a shoe object to hold the necessary number of decks for one simulation
+# I want to ensure I can simulate several shoes per execution of the code with a summarization of the results at the shoe level.
 
 #I don't believe a Deck class is necessary, as the Shoe class totally encapsulates its functionality
 
+#I created a functional Shoe as a single list with each Card as a Tuple of 2 strings, the Value and the Suit. The number of decks in the Shoe is a parameter
+# along with deck penetration, although deck penetration is not functional yet. It will be used to determine how prematurely a Shoe simulation ends.
+# To play real-time hands one Shoe can be used and for simulations of multiple shoes a simulator object will be created.
+
+#Now that I have my Shoe set up, I will want to create objects for the Player and the Dealer
+
 import itertools
 import random
-
 
 class Card:
 
@@ -23,40 +28,25 @@ class Card:
         self.value = value
         self.suit = suit
 
+class Shoe:
 
-class Deck:
+    def __init__ (self,number_of_decks = 6,deck_penetration = 1.0):
 
-    def __init__ (self):
-        self.new_deck = list(itertools.product(Card.list_of_values,Card.list_of_suits))
-        random.shuffle(self.new_deck)
+        self.number_of_decks = number_of_decks
+        self.deck_penetration = deck_penetration
+        self.new_shoe = []
+
+        for self.decks in range (0,number_of_decks):
+            self.new_shoe += (list(itertools.product(Card.list_of_values,Card.list_of_suits)))
+        
+        random.shuffle(self.new_shoe)
 
     def draw_card(self):
-        Card = self.new_deck.pop()
+        Card = self.new_shoe.pop()
         print(Card)
         return(Card)
 
 
-class Shoe:
-
-    def __init__(self,number_of_decks = 1,deck_penetration = 1.0):
-        self.number_of_decks = number_of_decks
-        self.deck_penetration = deck_penetration
-        self.new_shoe = []
-            
-        for self.decks in range(0,number_of_decks):
-            self.new_shoe.append(Deck())
-
-
-#Deck_1 = Deck()
-
-# cards_burned = 0
-
-# for cards in range(0,50): 
-#     Deck_1.draw_card()
-#     cards_burned += 1
-#print(len(Deck_1.new_deck))
-# print(Deck_1.new_deck)
-# print(cards_burned)
-
 Shoe_1=Shoe()
-print(len(Shoe_1.new_shoe(Deck())))
+Shoe_1.draw_card()
+print(len(Shoe_1.new_shoe))
