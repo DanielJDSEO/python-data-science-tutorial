83dd997d0293608e02bfd165f561243b21fdc029
Daniel <djd100@comcast.net>
2022-06-15 20:55:36 -0700

    Deleted it all, start from scratch (with comments)

--- lucky_lucky.py
+++ lucky_lucky.py
@@ -2,35 +2,6 @@
 # because the game rules are simpler. I will take this opportunity to re-create the class objects using additional things I've learned from watching
 # Precipio videos (special methods, the property decorator, getters/setters, subclasses, class vs. instance variables, etc.)
 
-import itertools
-import random
-
-class Card:
-    list_of_values = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
-    list_of_suits = ["spades","diamonds","hearts","clubs"]
-
-    values_dict = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10,
-                    'A':{True: 1, False: 11}}
-
-    def __init__(self,value,suit):
-        self.__value = value
-        self.__suit = suit
-
-
-class Shoe:
-    
-    def __init__ (self,number_of_decks = 6,deck_penetration = 1.0):
-
-        self.__number_of_decks = number_of_decks
-        self.__deck_penetration = deck_penetration
-        self.__new_shoe = []
-
-        for self.decks in range (0,number_of_decks):
-            self.__new_shoe += (list(itertools.product(Card.list_of_values,Card.list_of_suits)))
-        
-        random.shuffle(self.__new_shoe)
-
-    def draw_card(self):
-        Card = self.new_shoe.pop()
-        print(Card)
-        return(Card)
+#Now that I've made the instance variables 'private', I'm having trouble drawing a card from the Shoe instance.
+# I want to re-write the code so that Card objects are created and placed into the Shoe object, and then the Dealer and Player draw 
+# the Card objects from the Shoe object. Then, I want evaluations performed on the Card objects that the Dealer and Player are holding.
