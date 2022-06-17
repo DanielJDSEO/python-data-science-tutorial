388047b6d6d0a7f83ecd3e76a950bb0d5cd32257
Daniel <djd100@comcast.net>
2022-06-16 19:02:37 -0700

    brought all classes back here; made main instance

--- lucky_lucky.py
+++ lucky_lucky.py
@@ -6,3 +6,82 @@
 # I want to re-write the code so that Card objects are created and placed into the Shoe object, and then the Dealer and Player draw 
 # the Card objects from the Shoe object. Then, I want evaluations performed on the Card objects that the Dealer and Player are holding. I will create separate
 # files for the Card and Shoe classes.
+
+import itertools
+import random
+
+
+
+class lucky_lucky:
+
+    def __init__(self):
+        self.__s1 = Shoe()
+        self.__p1 = Player()
+
+    def draw_starting_hand(self):
+        self.__s1.deal_starting_hand()
+
+
+
+
+class Card:
+    
+    list_of_values = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+    list_of_suits = ["spades","diamonds","hearts","clubs"]
+
+    values_dict = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10,
+                    'A':{True: 1, False: 11}}
+
+    def __init__ (self,value,suit):
+        self.__value = value
+        self.__suit = suit
+
+
+class Shoe:
+    
+    def __init__ (self,number_of_decks = 6,deck_penetration = 1.0):
+
+        self.__number_of_decks = number_of_decks
+        self.__deck_penetration = deck_penetration
+        self.__new_shoe = []
+        self.__card = []
+
+        for self.decks in range (0,number_of_decks):
+            self.__new_shoe += (list(itertools.product(Card.list_of_values,Card.list_of_suits)))
+        random.shuffle(self.__new_shoe)
+
+    def draw_card(self):
+        __card = self.__new_shoe.pop()
+        print(len(self.__new_shoe))
+        print(__card)
+        return(__card)
+
+    def deal_starting_hand(self):
+        self.draw_card()
+        self.draw_card()
+
+
+class Player:
+    
+    def __init__ (self):
+        self.__bet = 0
+        self.__starting_hand = []
+        self.__current_hand = self.__starting_hand
+
+    def hit(self):
+        self.__current_hand.append(self.draw_card())
+        print(self.__current_hand)
+
+    # def draw_card(self):
+    #     Card = BJShoe.__new_shoe.pop()
+    #     print(Card)
+    #     return(Card)
+
+    def draw_starting_hand(self):
+        self.__starting_hand.append(self.draw_card()) 
+        self.__starting_hand.append(self.draw_card())
+
+
+game1 = lucky_lucky()
+
+game1.draw_starting_hand()
