aa105e2ab12a3185023ccce4269b54b996371111
Daniel <djd100@comcast.net>
2022-06-16 19:44:49 -0700

    successful pass of instance variables

--- lucky_lucky.py
+++ lucky_lucky.py
@@ -1,27 +1,9 @@
-#I am going to re-create what I've done for Blackjack so far but I'm going to build the Lucky Lucky minigame first. This will be much easier to simulate
-# because the game rules are simpler. I will take this opportunity to re-create the class objects using additional things I've learned from watching
-# Precipio videos (special methods, the property decorator, getters/setters, subclasses, class vs. instance variables, etc.)
-
-#Now that I've made the instance variables 'private', I'm having trouble drawing a card from the Shoe instance.
-# I want to re-write the code so that Card objects are created and placed into the Shoe object, and then the Dealer and Player draw 
-# the Card objects from the Shoe object. Then, I want evaluations performed on the Card objects that the Dealer and Player are holding. I will create separate
-# files for the Card and Shoe classes.
+#I using the Lucky Lucky class to run the game instance and initializing the Shoe and Player instances within the game instance. This should provide an
+# overarching instance to do additional actions: Pull cards from the Shoe instance, move them to the game instance, and then from the game instance
+# into the Player instance.
 
 import itertools
 import random
-
-
-
-class lucky_lucky:
-
-    def __init__(self):
-        self.__s1 = Shoe()
-        self.__p1 = Player()
-
-    def draw_starting_hand(self):
-        self.__s1.deal_starting_hand()
-
-
 
 
 class Card:
@@ -37,7 +19,7 @@ class Card:
         self.__suit = suit
 
 
-class Shoe:
+class Shoe(Card):
     
     def __init__ (self,number_of_decks = 6,deck_penetration = 1.0):
 
@@ -47,7 +29,7 @@ class Shoe:
         self.__card = []
 
         for self.decks in range (0,number_of_decks):
-            self.__new_shoe += (list(itertools.product(Card.list_of_values,Card.list_of_suits)))
+            self.__new_shoe += (list(itertools.product(self.list_of_values,self.list_of_suits)))
         random.shuffle(self.__new_shoe)
 
     def draw_card(self):
@@ -60,26 +42,39 @@ class Shoe:
         self.draw_card()
         self.draw_card()
 
-
 class Player:
     
     def __init__ (self):
         self.__bet = 0
         self.__starting_hand = []
-        self.__current_hand = self.__starting_hand
 
-    def hit(self):
-        self.__current_hand.append(self.draw_card())
-        print(self.__current_hand)
+class Dealer(Player):
+    def __init__(self):
+        pass
 
-    # def draw_card(self):
-    #     Card = BJShoe.__new_shoe.pop()
-    #     print(Card)
-    #     return(Card)
+
+class lucky_lucky(Shoe,Player):
+
+    def __init__(self):
+        self.__s1 = Shoe()
+        self.__p1 = Player()
+        self.__d1 = Dealer()
+        self.__p1holding = []
+        self.__d1holding = []
 
     def draw_starting_hand(self):
-        self.__starting_hand.append(self.draw_card()) 
-        self.__starting_hand.append(self.draw_card())
+        self.__p1holding += self.__s1.draw_card()
+        self.__p1holding += self.__s1.draw_card()
+        self.__d1holding += self.__s1.draw_card()
+        self.__d1holding += self.__s1.draw_card()
+        
+        self.__p1.__starting_hand = self.__p1holding
+        self.__d1.__starting_hand = self.__d1holding
+        print(self.__p1.__starting_hand)
+        print(self.__d1.__starting_hand)
+
+    
+    
 
 
 game1 = lucky_lucky()
